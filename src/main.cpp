0f719b54727a444984c6dd348ba64c2f97efa2f7
tommy <tomi.git@gmail.com>
2017-06-04 15:38:45 +0200

    Add cereal library

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &agentsNumber);
 
-    Agent agent(rank,agentsNumber);
+    Agent agent(rank, agentsNumber);
     agent.run();
 
     MPI_Finalize();
